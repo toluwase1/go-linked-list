b591444a5ca24f0a61efed569d1ba96f33e83d90
toluwase <toluwasethomas1@gmail.com>
2022-04-04 20:53:48 +0100

    delete value

--- main.go
+++ main.go
@@ -108,6 +108,31 @@ func (l *LinkedList) DeleteAt(pos int) error {
 	return nil
 }
 
+func (l *LinkedList) DeleteVal(val int) error {
+	ptr := l.head
+	if l.len == 0 {
+		fmt.Println("List is empty")
+		return errors.New("List is empty")
+	}
+	for i := 0; i < l.len; i++ {
+		if ptr.value == val {
+			if i > 0 {
+				prevNode := l.GetAt(i - 1)
+				prevNode.next = l.GetAt(i).next
+			} else {
+				l.head = ptr.next
+			}
+			l.len--
+			return nil
+		}
+		ptr = ptr.next
+	}
+	fmt.Println("Node not found")
+	return errors.New("Node not found")
+}
+
+
+
 func main()  {
 	l := LinkedList{}
 	l.Append(15)
